dd57e5f4f2539d6d472f2d7aa7e3b64db4065203
Rayed <rayed15900@gmail.com>
2023-04-25 20:05:17 +0600

    Structure

---
+++ CareerPortal/DAL/Models/CPContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class CPContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
