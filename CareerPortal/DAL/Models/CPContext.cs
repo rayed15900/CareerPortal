d72bd126159068e0f7fcc190ea8a10badd7aa1ff
unknown <alinur1160@gmail.com>
2023-04-25 20:42:28 +0600

    AdminProfile created

--- CareerPortal/DAL/Models/CPContext.cs
+++ CareerPortal/DAL/Models/CPContext.cs
@@ -11,5 +11,6 @@ namespace DAL.Models
     {
         public DbSet<User> Users { get; set; }
         public DbSet<ApplicantProfile> ApplicantProfiles { get; set; }
+        public DbSet<AdminProfile> AdminProfiles { get; set; }
     }
 }
