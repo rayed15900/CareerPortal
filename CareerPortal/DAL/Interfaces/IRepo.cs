2d99ab673fd0bd30a32cf603c861a24a0c11f8f2
Rayed <rayed15900@gmail.com>
2023-04-26 22:04:02 +0600

    modified

---
+++ CareerPortal/DAL/Interfaces/IRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IRepo<Type, ID, RET>
+    {
+        RET Create(Type obj);
+        List<Type> Read();
+        Type Read(ID iD);
+        RET Update(Type obj);
+        bool Delete(ID iD);
+    }
+}
