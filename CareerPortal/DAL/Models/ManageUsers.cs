04e9f01466cb0ffc50bfa6cf893bb5918852cc69
Alinur1 <alinur1160@gmail.com>
2023-04-26 23:36:42 +0600

    Pulling-Alinur

--- CareerPortal/DAL/Models/ManageUsers.cs
+++ CareerPortal/DAL/Models/ManageUsers.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +11,14 @@ namespace DAL.Models
 
     public class ManageUsers
     {
-
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("User")]
+        public int UId { get; set; }
+        public virtual ICollection<User> Users { get; set;}
+        public ManageUsers()
+        {
+            Users = new List<User>();
+        }
     }
 }
