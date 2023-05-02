23015eb1e425a7650241f3b107a7e13a7f1c34eb
Rayed <rayed15900@gmail.com>
2023-05-02 22:51:18 +0600

    demo

--- CareerPortal/DAL/Models/EmployerProfile.cs
+++ CareerPortal/DAL/Models/EmployerProfile.cs
@@ -28,14 +28,10 @@ namespace DAL.Models
         [StringLength(20)]
         public string Nationality { get; set; }
         [Required]
-        [StringLength(10, MinimumLength = 10)]
-        public string NationalID { get; set; }
-        [Required]
         public DateTime DateOfBirth { get; set; }
         [Required]
         public string Gender { get; set; }
 
-        
         [ForeignKey("user")]
         public int User_Id { get; set; }
         public virtual User user { get; set; }
